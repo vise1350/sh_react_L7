0dba09f23bb6763dbffe75e1c19eab3488868dcb
vise1350 <vise1350@gmail.com>
2023-07-05 12:35:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Lets learn React </h1>
+        <ExpenseItem></ExpenseItem>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+function ExpenseItem(){
+    return  <div>
+        <h2>
+            Expense Items
+        </h2>
+
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+
+
+
+
+
+    </div>
+    
+
+    }
+   
+
+export default ExpenseItem;
